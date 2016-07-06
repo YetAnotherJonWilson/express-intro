cffb93f27a5fa01a1cce442132e9f4b2803249a7
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-07-06 15:28:52 -0500

    working version

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-changes
+var express = require('express');
+var app = express();
+var port = 3000;
+
+
+app.listen(port, function onServerListen(){
+  console.log('Started Express Server on Port 3000');
+  console.log('Press Control-C to stop');
+});
+
+app.get('/', function(request, response){
+  response.send('Go Cubs!');
+});
